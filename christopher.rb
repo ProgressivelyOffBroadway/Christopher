a89e7921a1a8bc102cbfb5667d062df6fc2eef21
Ezra Goss <ezragoss@me.com>
2018-02-02 15:50:10 -0500

    Hopefully added logger

--- christopher.rb
+++ christopher.rb
@@ -1,27 +1,8 @@
 require 'rubygems'
 require 'sinatra/base'
 require 'json'
-require 'logger'
 
 #class Christopher < Sinatra::Base
-
-  # Logging
-  ::Logger.class_eval { alias :write :'<<' }
-  access_log = ::File.join(::File.dirname(::File.expand_path(__FILE__)),'..','log','access.log')
-  access_logger = ::Logger.new(access_log)
-  error_logger = ::File.new(::File.join(::File.dirname(::File.expand_path(__FILE__)),'..','log','error.log'),"a+")
-  error_logger.sync = true
-  
-  configure do
-    use ::Rack::CommonLogger, access_logger
-  end
-  
-  before {
-    env["rack.errors"] =  error_logger
-  }
-  
-
-
 
   get '/' do
     "Hello world!"
